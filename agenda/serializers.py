a7a0310e30703ea86184f7156c5fbd56c76014ed
Santos Nobre CRM <admin@santosnobre.com.br>
2026-02-23 21:54:41 +0000

    feat: TipoProcesso seeded, busca cliente Processos, processo+tipo Agenda, prazos urgentes Dashboard

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -4,9 +4,15 @@ from .models import Evento
 
 class EventoSerializer(serializers.ModelSerializer):
     responsavel_nome = serializers.StringRelatedField(source='responsavel')
-    processo_numero = serializers.StringRelatedField(source='processo')
+    processo_numero  = serializers.StringRelatedField(source='processo')
+    tipo_processo_nome = serializers.SerializerMethodField()
 
     class Meta:
         model = Evento
         fields = '__all__'
         read_only_fields = ['criado_em']
+
+    def get_tipo_processo_nome(self, obj):
+        if obj.processo and hasattr(obj.processo, 'tipo_processo') and obj.processo.tipo_processo:
+            return obj.processo.tipo_processo.nome
+        return None
