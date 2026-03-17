adf88649011d8bd09928efc04aadae7f7140046a
mauro mantilha <mauromantilha@gmail.com>
2026-03-16 22:20:25 -0300

    included allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -6,3 +6,9 @@ Este módulo existe para evitar quebra de imports antigos.
 """
 
 from crm_advocacia.settings import *  # noqa: F401,F403
+ALLOWED_HOSTS = [
+    'univesp-pi-group-12-production.up.railway.app',
+    '*.up.railway.app',
+    'localhost',
+    '127.0.0.1',
+]
